cfef2ce69d5020ff21e40a3200e94178170035ad
dehrhard <d.ehrhard@gastromatic.de>
2022-10-26 16:19:03 +0200

    chore: an update

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,14 +8,16 @@ export default function Home() {
         <title>Fresh App</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
+        drip
         <img
           src="/logo.svg"
           class="w-32 h-32"
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
+        drip
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
         </p>
         <Counter start={3} />
       </div>
